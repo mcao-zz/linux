cc3c9ba234d7f50a46bcd5cdd899ae1f01e3b473
Gustavo Walbon <gwalbon@linux.ibm.com>
2019-05-08 17:50:21 -0300

    Validating RHEL's
    Updateing of iKOTD project
    Bring updates of mapping.sh
    Fix the setup for rpmbuild

--- update.sh
+++ update.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-# Reading sources
+: update the submodules
+git submodule init
+
+:  Reading sources
 . ./iKOTD/mapping.sh
 
 : Reading options
@@ -20,19 +23,19 @@ done
 set -x
 
 if [[ ! -n "${DISTRO}" ]] ; then
-	echo "Wrong -d was not set."
+	echo "ERROR: Wrong -d was not set."
 	exit 1
 fi
 if [[ ! -n "${USERName}" ]]; then
-	echo "Wrong -n was not set."
+	echo "ERROR: Wrong -n was not set."
 	exit 1
 fi
 if [[ ! -n "${USEREmail}" ]]; then
-	echo "Wrong -e was not set."
+	echo "ERROR: Wrong -e was not set."
 	exit 1
 fi
 if [[ ! -n "${FTP3}" ]]; then
-	echo "Wrong -f was not set."
+	echo "ERROR: Wrong -f was not set."
 	exit 1
 fi
 
@@ -41,40 +44,39 @@ git config --global user.name "${USERName}"
 git fetch --all
 
 : Checking if the remote branch is already added
-if git branch          | grep "${ftp3[${DISTRO}]}" &>/dev/null; then
-	if git branch | grep "^\* ${ftp3[${DISTRO}]}" &>/dev/null; then
+if git branch         | grep -q "${ftp3[${DISTRO}]}" &>/dev/null; then
+	if git branch | grep -q "^\* ${ftp3[${DISTRO}]}" &>/dev/null; then
 		: Current branch
 	fi
 	: Change to another branch which already exist
 	git checkout ${ftp3[${DISTRO}]}
-elif ! git branch -avv | grep "remotes/origin/${ftp3[${DISTRO}]}" &>/dev/null; then
+
+elif ! git branch -avv | grep -q "remotes/origin/${ftp3[${DISTRO}]}" &>/dev/null; then
 	: Creating new branch
 	git checkout -b ${ftp3[${DISTRO}]} origin/master
+	: Push it to remote
 	git push -u origin ${ftp3[${DISTRO}]}
 else
-	git checkout -b ${ftp3[${DISTRO}]} origin/master
-	git push -u origin ${ftp3[${DISTRO}]}
+	: Checkout to remote branch
+	git checkout -b ${ftp3[${DISTRO}]} origin/${ftp3[${DISTRO}]}
 fi
 
 ROOT="$(pwd)"
 RH_PASSPORT="${FTP3}" bash -x ./iKOTD/get_latest.sh ${ftp3[${DISTRO}]}
 
 : Looking for the latest downloaded file for build
-for i in $(find ${ROOT} -iname 'kernel*.src.rpm');
-do
-	KRN_SRC_Filename="${i}"
-done
+KRN_SRC_Filename="$(echo kernel.*src.rpm | sort -V | tail -n1)"
 
 KRN_SRC_Filename="$(echo ${KRN_SRC_Filename} | rev | cut -d'/' -f1 | rev)"
 
-if [[ ! -n "${KRN_SRC_Filename}" ]]; then
-	echo "Kernel source was not found."
+if [[ ! -e "${KRN_SRC_Filename}" ]]; then
+	echo "ERROR: Kernel source was not found."
 	exit 1
 fi
 
 : Checking if package was already committed
-if git log --oneline  | grep "${KRN_SRC_Filename}" > /dev/null; then
-	echo "It's already added"
+if git log --oneline  | grep -q "${KRN_SRC_Filename}" > /dev/null; then
+	echo "FINISH: It's already added"
 	exit 0
 fi
 
@@ -107,5 +109,4 @@ git commit -s -a -m "[${ftp3[${DISTRO}]}] ${KRN_SRC_Filename}"
 git push
 
 
-
-
+echo FINISH
