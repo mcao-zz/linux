9b108608583c6ab1efa527b13c9e7d46c23550b7
Mingming Cao <mmc@linux.ibm.com>
2026-05-15 22:15:15 -0700

    Revert "ibmveth: Add debug logging for buffer replenishment hypercall"
    This reverts commit 37a418d4b956f15663f96286415d699e58bfd9de.

--- drivers/net/ethernet/ibm/ibmveth.c
+++ drivers/net/ethernet/ibm/ibmveth.c
@@ -466,7 +466,7 @@ static int ibmveth_disable_irq(struct ibmveth_adapter *adapter, int queue_index)
 
 		if (rc == H_PARAMETER) {
 			netdev_err(adapter->netdev,
-				   "H_PARAMETER from H_DISABLE_VIO_INTERRUPT: queue=%d virq=%lu hwirq=%lu unit_address=0x%lx\n",
+				   "H_PARAMETER from H_DISABLE_VIO_INTERRUPT: queue=%d virq=%lu hwirq=%lu unit_address=0x%x\n",
 				   queue_index, irq, hwirq, adapter->vdev->unit_address);
 			return 0;  /* Treat as success to stop interrupt storm */
 		}
@@ -523,7 +523,7 @@ static int ibmveth_enable_irq(struct ibmveth_adapter *adapter, int queue_index)
 
 		if (rc == H_PARAMETER) {
 			netdev_err(adapter->netdev,
-				   "H_PARAMETER from H_ENABLE_VIO_INTERRUPT: queue=%d virq=%lu hwirq=%lu unit_address=0x%lx\n",
+				   "H_PARAMETER from H_ENABLE_VIO_INTERRUPT: queue=%d virq=%lu hwirq=%lu unit_address=0x%x\n",
 				   queue_index, irq, hwirq, adapter->vdev->unit_address);
 			return 0;  /* Treat as success */
 		}
@@ -744,14 +744,6 @@ static int ibmveth_add_logical_lan_buffers(struct ibmveth_adapter *adapter,
 			ioba1112 = descs[10].fields.address;
 		if (filled >= 12)
 			ioba1112 |= ((unsigned long)descs[11].fields.address << 32);
-
-		netdev_info(adapter->netdev,
-			    "DEBUG: h_add_logical_lan_buffers_queue: unit=0x%x qhandle=0x%llx bufsznum=0x%lx filled=%d buffsize=%lu queue=%d\n",
-			    vdev->unit_address, adapter->queue_handle[queue_index],
-			    buffersznum, filled, buff_size, queue_index);
-		netdev_info(adapter->netdev,
-			    "DEBUG: ioba12=0x%lx ioba34=0x%lx ioba56=0x%lx ioba78=0x%lx ioba910=0x%lx ioba1112=0x%lx\n",
-			    ioba12, ioba34, ioba56, ioba78, ioba910, ioba1112);
 
 		rc = h_add_logical_lan_buffers_queue(vdev->unit_address,
 						     adapter->queue_handle[queue_index],
